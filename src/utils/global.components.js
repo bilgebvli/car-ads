74de1c1d6528c7837a964ccb85c12fb005564d5e
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-19 01:24:46 +0300

    Utils file was created.

---
+++ src/utils/global.components.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+import CarAdButton from '@/components/ui/CarAdButton';
+import CarAdPageLoader from '@/components/ui/CarAdPageLoader';
+import CarAdCard from '@/components/ui/CarAdCard';
+import CarAdImage from '@/components/ui/CarAdImage';
+import CarAdPagination from '@/components/ui/CarAdPagination';
+import CarAdDivider from '@/components/ui/CarAdDivider';
+import CarAdSwiperSlide from '@/components/ui/CarAdSwiperSlide';
+
+Vue.component('CarAdButton', CarAdButton);
+Vue.component('CarAdPageLoader', CarAdPageLoader);
+Vue.component('CarAdCard', CarAdCard);
+Vue.component('CarAdImage', CarAdImage);
+Vue.component('CarAdPagination', CarAdPagination);
+Vue.component('CarAdDivider', CarAdDivider);
+Vue.component('CarAdSwiperSlide', CarAdSwiperSlide);
