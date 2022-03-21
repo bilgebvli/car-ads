1b51347cdda55dfbfb213040e05dcb252fabb79a
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-22 02:32:43 +0300

    Divider component was created

---
+++ src/components/ui/CarAdDivider/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <hr v-bind="dividerOptions" />
+</template>
+
+<script>
+export default {
+  name: 'CarAdDivider',
+  props: {
+    color: {
+      type: String,
+    },
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    dividerOptions() {
+      return {
+        class: {
+          [this.componentName]: true,
+          [`${this.componentName}--${this.color}`]: true,
+          [`${this.componentName}--vertical`]: this.vertical,
+        },
+      };
+    },
+  },
+};
+</script>
