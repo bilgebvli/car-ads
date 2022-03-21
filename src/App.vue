87ee5b0afa2212c55022181c45656e1226e87fad
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-22 02:07:17 +0300

    App file wad edited for loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
+    <car-ad-preloader v-if="isAppLoading"></car-ad-preloader>
     <router-view />
   </div>
 </template>
 
 <script>
+import CarAdPreloader from '@/components/ui/CarAdPreLoader';
+
 export default {
   name: 'App',
+  components: {
+    CarAdPreloader,
+  },
+  computed: {
+    isAppLoading() {
+      return this.$store.getters.getIsAppLoading;
+    },
+  },
 };
 </script>
