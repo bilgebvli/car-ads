28deaa8672dac2995f7b0b6ca947283844ea82df
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-22 02:39:23 +0300

    Modal component was created

---
+++ src/components/ui/CarAdModal/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="modal" :class="componentName" @click="clickHandler">
+    <div :class="contentOptions">
+      <div class="text-right">
+        <span :class="`${componentName}__close`" @click="close">&times;</span>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarAdModal',
+  computed: {
+    contentOptions() {
+      return {
+        [`${this.componentName}__model-content`]: true,
+        'rounded-lg': true,
+      };
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+    clickHandler(event) {
+      if (event.target == document.getElementById('modal')) this.close();
+    },
+  },
+};
+</script>
