ee9f1e38dd929c6f0e55902fd8fcf263669a5492
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-22 02:34:14 +0300

    List component was created

---
+++ src/components/ui/CarAdList/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="list-style-none p-0">
+    <li v-for="(item, index) of listItems" :key="index">
+      <car-ad-divider></car-ad-divider>
+      <span class="font-weight-bolder w-50 d-inline-block"
+        >{{ item.name }}:</span
+      >
+      <span>{{ item.value }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CarAdList',
+  props: {
+    listItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
