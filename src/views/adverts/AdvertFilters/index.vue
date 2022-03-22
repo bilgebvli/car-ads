c42344161acfdcfffcbe11d8198f7f066d327864
bilgebvli <35532766+bilgebvli@users.noreply.github.com>
2022-03-22 03:05:08 +0300

    Adverts page was created

---
+++ src/views/adverts/AdvertFilters/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <car-ad-select
+      v-model="sortValue"
+      :items="sortItems"
+      class="p-1"
+      @change="change"
+    ></car-ad-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdvertFilters',
+  data() {
+    return {
+      sortItems: [
+        {
+          name: this.$t('adverts.sortItems.price'),
+          value: 'price',
+        },
+        {
+          name: this.$t('adverts.sortItems.date'),
+          value: 'date',
+        },
+        {
+          name: this.$t('adverts.sortItems.year'),
+          value: 'year',
+        },
+      ],
+      sortValue: 'year',
+    };
+  },
+  methods: {
+    change(sortValue) {
+      this.$emit('change-sorting', { sortValue });
+    },
+  },
+};
+</script>
---
+++ src/views/adverts/AdvertList/AdvertItemCard/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <car-ad-image
+      :src="$mediaManager.getThumb({ media: advertItem.photo })"
+      :alt="advertItem.title"
+      class="card-img mb-4"
+    ></car-ad-image>
+    <h5 class="card-title text-clipping">{{ advertItem.title }}</h5>
+    <p v-text="properties"></p>
+    <p class="d-inline mr-2 text-danger font-weight-bolder">
+      {{ $n(advertItem.price, 'currency') }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdvertItemCard',
+  props: {
+    advertItem: {
+      type: Object,
+    },
+  },
+  computed: {
+    properties() {
+      return this.advertItem.properties.map((item) => item.value).join(' | ');
+    },
+  },
+};
+</script>
